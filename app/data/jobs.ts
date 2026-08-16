264656ada3f128f82080c8f1a94df12986d4c09a
Eleni-tadese <elenitade1221@gmail.com>
2026-08-16 22:08:29 +0300

    feat: add job model and dummy data

---
+++ app/data/jobs.ts
@@ -0,0 +1,58 @@
+import { Job } from "../types/job";
+
+export const jobs: Job[] = [
+  {
+    id: "1",
+    title: "Frontend Developer",
+    company: "Tech Solutions",
+    location: "Addis Ababa",
+    description:
+      "Build responsive user interfaces using React, Next.js, and modern frontend tools.",
+    image: "https://cdn.simpleicons.org/github",
+    categories: ["Frontend", "Full Time"]
+  },
+
+  {
+    id: "2",
+    title: "Backend Developer",
+    company: "Cloud Systems",
+    location: "Remote",
+    description:
+      "Develop APIs, manage databases, and create scalable backend services.",
+    image: "https://cdn.simpleicons.org/github",
+    categories: ["Backend", "Remote"]
+  },
+
+  {
+    id: "3",
+    title: "UI/UX Designer",
+    company: "Creative Studio",
+    location: "Adama",
+    description:
+      "Design beautiful user experiences and create modern web application designs.",
+    image: "https://cdn.simpleicons.org/github",
+    categories: ["Design", "Part Time"]
+  },
+
+  {
+    id: "4",
+    title: "Full Stack Developer",
+    company: "Digital Africa",
+    location: "Addis Ababa",
+    description:
+      "Work on both frontend and backend applications using JavaScript technologies.",
+    image: "https://cdn.simpleicons.org/github",
+    categories: ["Full Stack", "Full Time"]
+  },
+
+  {
+    id: "5",
+    title: "Mobile App Developer",
+    company: "App Factory",
+    location: "Remote",
+    description:
+      "Create mobile applications and integrate APIs with modern frameworks.",
+    image: "https://cdn.simpleicons.org/github",
+    categories: ["Mobile", "Remote"]
+  }
+];
